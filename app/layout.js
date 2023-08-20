f935536981bdb1355395a4013071d82b4fd2b2d5
calebareeveso <calebareeveso7@gmail.com>
2023-08-20 08:42:34 +0100

    fix: remove api cache

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
   description:
     "Qna is a web application that enhances memory retention through active recall. Users create personalized question-and-answer collections, and the web app utilizes active recall techniques to prompt information retrieval, boosting long-term memory. QnA's intelligent review system highlights questions needing review after seven days or a set due date, ensuring effective learning and memory reinforcement.",
 };
-
+export const revalidate = 0;
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
--- app/page.js
+++ app/page.js
@@ -1,17 +1,12 @@
 import { redirect } from "next/navigation";
 import Script from "next/script";
 // component
-import Nav from "@/component/nav";
-import Header from "@/component/header";
-import Prompt from "@/component/qna";
-import PromptEditor from "@/component/editor";
-
 import Main from "@/component/main";
-import VisitorsWarning from "@/component/visitorsWarning";
+
 // lib
-import formattedDate from "@/lib/fDate";
 import getAllQnA from "@/lib/fetch/get/allQnA";
 import getAllCollection from "@/lib/fetch/get/allCollection";
+export const revalidate = 0;
 export default async function Page() {
   const qna = await getAllQnA("about");
   const collections = await getAllCollection();
