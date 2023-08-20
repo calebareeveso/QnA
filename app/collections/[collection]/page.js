9f2b3a833b0da2c2a9dbd7f810b7b11c9f88a146
calebareeveso <calebareeveso7@gmail.com>
2023-08-20 16:36:01 +0100

    fix: remove api cache

--- app/collections/[collection]/page.js
+++ app/collections/[collection]/page.js
@@ -6,10 +6,21 @@ import Main from "@/component/main";
 // lib
 import getAllQnA from "@/lib/fetch/get/allQnA";
 import getAllCollection from "@/lib/fetch/get/allCollection";
-export const revalidate = 0;
+import server from "@/lib/server";
 export default async function Page({ params }) {
-  const qna = await getAllQnA(params.collection);
-  const collections = await getAllCollection();
+  // const qna = await getAllQnA(params.collection);
+  const qnaresponse = await fetch(`${server}/api/qna/${params.collection}`, {
+    cache: "no-store",
+  });
+  const qnadata = await qnaresponse.json();
+  const qna = qnadata.qna;
+  // const collections = await getAllCollection();
+  const res = await fetch(`${server}/api/collection`, { cache: "no-store" });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch collection");
+  }
+  const collections = await res.json();
   const isSlugValid = collections.some(
     (item) => item.title === params.collection
   );
